629adacc8a82fbe138d961a0406ca13fe31bc192
SuhaibulHassan <164619337+SuhaibulHassan@users.noreply.github.com>
2025-08-06 22:33:41 +0530

    Create Category.js

---
+++ Backend/models/Category.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose"
+const Schema = mongoose.Schema;
+const CategorySchema = new Schema({
+    name: {
+        type: String, required:
+            true
+    },
+});
+// module.exports = mongoose.model("Category", CategorySchema);
+const Category = mongoose.model("Category", CategorySchema);
+export default Category;
